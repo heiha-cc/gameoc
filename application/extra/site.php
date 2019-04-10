645203bb8ab1fe7502352c6a051f2e0f7992e9bd
number5wh <1031158073@qq.com>
2019-04-10 18:49:42 +0800

    ee

---
+++ application/extra/site.php
@@ -0,0 +1,16 @@
+<?php
+
+return array (
+  'codeid' => '48a6ebac4ebc6642d68c217fca33eb4d',
+  'codekey' => '4f1c085290bec5afdc54df73535fc361',
+  'url' => 'http://api.game2019.net',
+  'appkey' => '22122',
+  'appsecret' => '2121212',
+  'mail_type' => '2',
+  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_port' => '465',
+  'mail_smtp_user' => '1000011122',
+  'mail_smtp_pass' => 'password',
+  'mail_verify_type' => '2',
+  'mail_from' => '10000@qq.com',
+);
